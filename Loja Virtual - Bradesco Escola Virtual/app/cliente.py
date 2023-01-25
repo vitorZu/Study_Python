283a7873c99a9ff820b806b373fa6b0f441bb65f
Vitor Zucon <vitor.zucon25@gmail.com>
2023-01-25 12:43:40 -0300

    first commit

---
+++ Loja Virtual - Bradesco Escola Virtual/app/cliente.py
@@ -0,0 +1,4 @@
+class Cliente:
+    def __init__(self,name,fone) -> None:
+        self.nome = name
+        self.telefone = fone
---
+++ Loja Virtual - Bradesco Escola Virtual/app/main.py
@@ -0,0 +1,10 @@
+
+
+class Main:
+    from cliente import Cliente
+    
+
+    c1 = Cliente("João", "114444-2222")
+
+    print(c1)
+    print(c1.nome," e ", c1.telefone)
